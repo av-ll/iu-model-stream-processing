aa5fd33c0fcfc432ac9389647321635049779caf
av-ll <104671568+av-ll@users.noreply.github.com>
2022-07-28 21:39:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def predict(predict):
     time_of_production = str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     cur.execute("""
             INSERT INTO new_sensor_data VALUES (%s,%s,%s,%s);
-            """,(time_of_production,received[keys[0]],received[keys[1]],received[keys[2]]))
+            """,(1,time_of_production,received[keys[0]],received[keys[1]],received[keys[2]]))
     conn.commit()
     return render_template('index.html',temperature=temp,humidity=humid,volume=vol,prediction_text='Probability of anomaly is {}%'.format(output),prediction = prediction_label)
 
